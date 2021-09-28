25af2c631fec25e86d8e80da92e39b1c705cbd36
centmount <78281830+centmount@users.noreply.github.com>
2021-09-28 12:28:14 +0900

    Update twitter_analysis_streamlit.py

--- twitter_data_analysis/twitter_analysis_streamlit.py
+++ twitter_data_analysis/twitter_analysis_streamlit.py
@@ -22,10 +22,11 @@ from datetime import datetime, timedelta
 import os
 
 # パスワードでログイン
-value = st.text_input('パスワードを入力してください:', value='', type='password')
-if value != st.secrets['password']:
+def login():
+    value = st.text_input('パスワードを入力してください:', value='', type='password')
+    while value != st.secrets['password']:
     st.error('正しいパスワードを入力してください')
-    return
+login()
 
 # 現在時刻
 now_time = datetime.now() + timedelta(hours=9)
