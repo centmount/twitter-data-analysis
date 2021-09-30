0b081c9b3c68e120dbbd2e30e4b94105ba04e6aa
centmount <nakayama1s@kb4.so-net.ne.jp>
2021-09-30 22:42:37 +0900

    頻出ワードのコード修正

--- twitter_data_analysis/twitter_analysis_streamlit.py
+++ twitter_data_analysis/twitter_analysis_streamlit.py
@@ -331,6 +331,7 @@ if genre == 'フォロワーごと フォロワー数 最新':
 
 st.write('フォロワー プロフィール欄の頻出ワードランキング (2017/4～) :google colabで分析')
 
+df_wordrank = df_wordrank.rename(columns={'Unnamed:0': 'word'})
 st.dataframe(df_wordrank, width=1000, height=600)
 
 csv3 = convert_df(df_wordrank)
@@ -341,7 +342,7 @@ st.markdown('''
 ''')
 
 # フォロワーのプロフィール欄ランキンググラフ作成用の辞書
-y = df_wordrank.index
+y = df_wordrank['word']
 x = df_wordrank['count']
 source = ColumnDataSource(data = dict(x = x, y = y))
 
